25dba61cf9f9b836593306388d1dc42306b040c7
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-04 19:17:58 -0300

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { FunctionComponent } from 'react';
 import Image from 'next/image';
+import { Ubuntu } from 'next/font/google';
+
+import { twMerge } from 'tailwind-merge';
+
+const ubuntu = Ubuntu({
+  weight: '500'
+});
 
 const Home: FunctionComponent = () => {
   return (
-    <main className="relative flex h-[100dvh] w-screen flex-col">
-      <h1>Task-it</h1>
-      <Image className="w-96" width={100} height={100} src="favicon.svg" alt="fdfd" />
+    <main className="relative flex h-[100dvh] w-screen flex-row items-center justify-center space-x-8">
+      <Image className="w-40" width={100} height={100} src="favicon.svg" alt="Task-it logo" />
+      <h1 className={twMerge('text-8xl', ubuntu.className)}>Task-it</h1>
     </main>
   );
 };
