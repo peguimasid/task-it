a08af3bf614bc9e880eef7780ee038e88f442f95
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-05 19:41:54 -0300

    add some configurations

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,18 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type AuthOptions } from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
 
-const handler = NextAuth({
-  providers: []
-});
+export const authOptions: AuthOptions = {
+  providers: [
+    // TODO: Add type safety to environment variables
+    DiscordProvider({
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!
+    })
+  ]
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
