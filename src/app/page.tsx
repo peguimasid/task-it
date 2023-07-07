78e8db428a2a1fdd850940873346037dc1849137
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-07 10:16:12 -0300

    print user info in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Ubuntu } from 'next/font/google';
 import { twMerge } from 'tailwind-merge';
 
 import { SignOutButton } from './components/sign-out-button';
+import { getServerSession } from 'next-auth';
 
 const ubuntu = Ubuntu({
   subsets: ['latin'],
@@ -12,12 +13,16 @@ const ubuntu = Ubuntu({
 });
 
 const Home = async () => {
+  const session = await getServerSession();
+
   return (
     <main className="relative flex h-[100dvh] w-screen flex-col items-center justify-center space-x-8">
       <div className="flex flex-row items-center justify-center space-x-8">
         <Image className="w-40" width={100} height={100} src="favicon.svg" alt="Task-it logo" />
         <h1 className={twMerge('text-8xl', ubuntu.className)}>Task-it</h1>
       </div>
+      <h1>Username: {session?.user?.name}</h1>
+      <h1>Email: {session?.user?.email}</h1>
       <SignOutButton />
     </main>
   );
