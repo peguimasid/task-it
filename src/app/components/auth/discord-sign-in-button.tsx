f9bb0f96e6dd8f62b3ffa2096482e575f9e51b62
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-07 16:16:21 -0300

    styling login page

--- src/app/components/auth/discord-sign-in-button.tsx
+++ src/app/components/auth/discord-sign-in-button.tsx
@@ -1,15 +1,16 @@
 'use client';
 
+import { FunctionComponent } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 
-const DiscordSignInButton = () => {
+const DiscordSignInButton: FunctionComponent = () => {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') ?? '/';
 
   return (
     <button
-      className="flex w-full items-center justify-center bg-purple-700"
+      className="flex w-full items-center justify-center rounded-md bg-purple-700 py-2"
       onClick={() => signIn('discord', { callbackUrl })}
     >
       Continue with Discord
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,11 @@ import DiscordSignInButton from '@/app/components/auth/discord-sign-in-button';
 const SignInPage = () => {
   return (
     <main className="flex h-[100dvh] w-screen flex-col items-center justify-center space-x-8 space-y-5">
-      <div className="flex max-w-sm flex-col justify-center space-y-3">
-        <h1>Login to you account</h1>
-        <DiscordSignInButton />
+      <div className="flex w-full max-w-sm flex-col items-center justify-center space-y-3">
+        <h1 className="text-4xl">Login to your account</h1>
+        <div className="flex w-full flex-col space-y-3 rounded-lg bg-gray-900 p-5">
+          <DiscordSignInButton />
+        </div>
       </div>
     </main>
   );
