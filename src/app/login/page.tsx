17f9800d4c306edf1831810e64f970a55b55c3be
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-07 18:03:33 -0300

    make login border thiner

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ const SignInPage = () => {
     <main className="flex h-[100dvh] w-screen flex-col items-center justify-center space-x-8 space-y-5">
       <div className="flex w-full max-w-sm flex-col items-center justify-center space-y-3">
         <h1 className="text-3xl">Login to your account</h1>
-        <div className="flex w-full flex-col space-y-3 rounded-lg border-2 border-solid border-gray-600 p-5">
+        <div className="flex w-full flex-col space-y-3 rounded-lg border border-solid border-gray-600 p-5">
           <SignInButton
             iconSrc="icons/discord.svg"
             providerName="Discord"
