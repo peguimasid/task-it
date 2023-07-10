1030b9f5130835dfa70f0e3faa606f714bd905b9
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-10 18:50:43 -0300

    make home page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,22 +18,26 @@ const Page: FunctionComponent = () => {
     <main className="flex h-[100dvh] w-screen flex-col">
       <TopBar showLoginButton />
       <section className="flex h-full flex-col items-center justify-center space-y-12">
-        <div className="flex flex-col items-center justify-center space-y-3">
-          <div className="flex items-center justify-center space-x-8">
-            <h1 className={twMerge('text-7xl font-extrabold', ubuntu.className)}>Welcome to Task-it</h1>
+        <div className="flex flex-col items-center justify-center space-y-3 px-5">
+          <div className="flex items-center justify-center space-x-4">
+            <h1 className={twMerge('text-3xl font-extrabold sm:text-5xl md:text-6xl lg:text-7xl', ubuntu.className)}>
+              Welcome to Task-it
+            </h1>
             <Image
-              className="pointer-events-none w-20 select-none"
+              className="pointer-events-none w-8 select-none sm:w-10 md:w-14 lg:w-20"
               width={100}
               height={100}
               src="favicon.svg"
               alt="Task-it logo"
             />
           </div>
-          <p className="text-xl text-gray-500">Keeping project management simple as should be</p>
+          <p className="text-center text-gray-500 md:text-lg lg:text-xl">
+            Keeping project management simple as should be
+          </p>
         </div>
         <Link className="group relative" href="/login?callbackUrl=/projects">
           <div className="absolute inset-0 rounded-lg blur-md transition-all duration-300 group-hover:bg-primary" />
-          <div className="relative flex flex-row items-center justify-center space-x-2 rounded-lg bg-gradient-to-r from-primary to-secondary px-8 py-4 font-semibold">
+          <div className="relative flex flex-row items-center justify-center space-x-2 rounded-lg bg-gradient-to-r from-primary to-secondary px-5 py-3 font-semibold sm:px-8 sm:py-4">
             <p>Sign up for free</p>
             <Image
               className="pointer-events-none w-4 select-none"
