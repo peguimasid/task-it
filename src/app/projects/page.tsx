babe4a65aeae802323f31a3040ae8b33450aba7d
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-10 20:11:05 -0300

    get user projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
-import { getServerAuthSession } from '@/server/auth';
 import { Metadata } from 'next';
 import { SignOutButton } from '../../components/auth/sign-out-button';
+import { prisma } from '@/server/prisma';
+import { getServerAuthSession } from '@/server/auth';
 
 export const metadata: Metadata = {
   title: 'Task-it | Projects'
@@ -11,10 +12,17 @@ const Page = async () => {
 
   if (!session?.user) return null;
 
+  const user = await prisma.user.findUnique({
+    where: { id: session.user.id },
+    include: { projects: true }
+  });
+
+  console.log(user?.projects);
+
   return (
     <main className="flex h-[100dvh] w-screen flex-col items-center justify-center space-x-8 space-y-5">
       <div className="flex flex-col items-center justify-center space-y-3">
-        <h1>List user {session.user.name} projects</h1>
+        <h1>List user projects</h1>
         <SignOutButton />
       </div>
     </main>
