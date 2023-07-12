2c97c91b55ff948b5ec1c0366d88ac87adb60f3a
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-12 19:23:35 -0300

    some styles changes

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -19,16 +19,21 @@ interface TopBarProps {
 export const TopBar: FunctionComponent<TopBarProps> = ({ showLoginButton = false }) => {
   return (
     <header className="sticky inset-0 z-10 flex w-full border-b border-gray-600 backdrop-blur-md">
-      <div className="mx-auto flex w-full max-w-7xl flex-row items-center justify-between px-5 py-4">
+      <div className="mx-auto flex w-full max-w-7xl flex-row items-center px-5 py-4">
         <Link href="/" className="flex flex-row items-center justify-center space-x-2 py-2">
           <Image width={90} height={90} src="favicon.svg" alt="Task-it logo" className="h-auto w-8" />
           <h1 className={twMerge('text-xl font-extrabold', ubuntu.className)}>Task-it</h1>
         </Link>
-        {showLoginButton && (
-          <Link className="rounded-lg border-2 border-secondary px-8 py-2 text-sm" href="/login?callbackUrl=/projects">
-            Login
-          </Link>
-        )}
+        <div className="ml-auto flex flex-row space-x-4">
+          {showLoginButton && (
+            <Link
+              className="rounded-lg border-2 border-secondary px-8 py-2 text-sm"
+              href="/login?callbackUrl=/projects"
+            >
+              Login
+            </Link>
+          )}
+        </div>
       </div>
     </header>
   );
