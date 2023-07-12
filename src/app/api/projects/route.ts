97292056cf0f7f714d2b5b37791f81e117c19921
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-12 14:54:56 -0300

    load user projects

--- src/app/api/projects/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function GET(request: NextRequest, response: NextResponse) {
-  return NextResponse.json({ ok: true });
-}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,34 @@
 import { Metadata } from 'next';
 import { SignOutButton } from '../../components/auth/sign-out-button';
 import { getServerAuthSession } from '@/server/auth';
+import { prisma } from '@/server/prisma';
 
 export const metadata: Metadata = {
   title: 'Task-it | Projects'
 };
 
-const Page = async () => {
+const getUserProjects = async () => {
   const session = await getServerAuthSession();
 
-  if (!session?.user) return null;
+  if (!session) return null;
+
+  const data = await prisma.user.findUnique({
+    where: { id: session.user.id },
+    include: { projects: true }
+  });
+
+  return data?.projects ?? [];
+};
+
+const Page = async () => {
+  const usersProjects = await getUserProjects();
+
+  console.log(usersProjects);
 
   return (
     <main className="flex h-[100dvh] w-screen flex-col items-center justify-center space-x-8 space-y-5">
       <div className="flex flex-col items-center justify-center space-y-3">
-        <h1>List user {session.user.name} projects</h1>
+        <h1>List user projects</h1>
         <SignOutButton />
       </div>
     </main>
