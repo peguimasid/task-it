4fc2d71376715a5807c4fc43701c13e41606e8e2
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-21 18:24:00 -0300

    override background paper color

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,20 @@
 import { createTheme } from '@mui/material/styles';
 
+import { zinc } from 'tailwindcss/colors';
+
 export const darkTheme = createTheme({
   components: {
     MuiButton: {
       styleOverrides: {
         root: {
           borderRadius: '9999px'
+        }
+      }
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          background: zinc[800]
         }
       }
     }
