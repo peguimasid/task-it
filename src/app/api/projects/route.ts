838ecbf092fe58db9f78e7058b4de62f9ddff397
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-22 00:13:33 -0300

    add fake delay

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return NextResponse.json({ coming_from_api: await request.json() });
 }
