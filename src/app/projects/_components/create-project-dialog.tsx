b17eeea84de3d1244b526b332ee63088f35aedcd
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-30 11:52:15 -0300

    implement dialog header

--- src/app/projects/_components/create-project-dialog.tsx
+++ src/app/projects/_components/create-project-dialog.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { FunctionComponent, useCallback, useMemo } from 'react';
+import { FunctionComponent, useCallback, useMemo, Fragment } from 'react';
+
+import { Dialog, Transition } from '@headlessui/react';
 
 import { useRouter } from 'next/navigation';
 
@@ -84,20 +86,42 @@ export const CreateProjectDialog: FunctionComponent<CreateProjectDialogProps> = 
   const onSubmit = useCallback((formData: FormValues) => mutate(formData), [mutate]);
 
   return (
-    <dialog open={open} onClose={handleCloseDialog}>
-      <DialogHeader onClose={handleCloseDialog}>Create Project</DialogHeader>
-      <form
-        name="createProjectForm"
-        noValidate
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col space-y-3 py-2"
-      >
-        <Controller name="name" control={control} render={({ field }) => <input {...field} type="text" />} />
-        <Controller name="description" control={control} render={({ field }) => <input {...field} type="text" />} />
-        <Button type="submit" disabled={isSubmitButtonDisabled} loading={isLoading}>
-          Send
-        </Button>
-      </form>
-    </dialog>
+    <Transition appear show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={handleCloseDialog}>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-200"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-zinc-900 p-6 text-left align-middle shadow-xl transition-all">
+                <DialogHeader onClose={handleCloseDialog}>Dialog Title</DialogHeader>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Your payment has been successfully submitted. Sent you an email with all of the details of your
+                    order.
+                  </p>
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    onClick={handleCloseDialog}
+                  >
+                    Got it, thanks!
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
--- src/components/dialog-header.tsx
+++ src/components/dialog-header.tsx
@@ -1,3 +1,5 @@
+import { Dialog } from '@headlessui/react';
+import { XMarkIcon } from '@heroicons/react/20/solid';
 import { FunctionComponent, PropsWithChildren } from 'react';
 
 interface DialogHeaderProps extends PropsWithChildren {
@@ -5,5 +7,14 @@ interface DialogHeaderProps extends PropsWithChildren {
 }
 
 export const DialogHeader: FunctionComponent<DialogHeaderProps> = ({ children, onClose }) => {
-  return <div>DIALOG HEDERS</div>;
+  return (
+    <Dialog.Title as="h3" className="-mt-2 flex items-center justify-between text-ellipsis whitespace-nowrap">
+      <p className="flex-grow-1 text-lg">{children}</p>
+      {!!onClose && (
+        <button aria-label="close" className="flex-shrink-0 rounded-full p-1 hover:bg-zinc-800" onClick={onClose}>
+          <XMarkIcon className="h-7 w-7" />
+        </button>
+      )}
+    </Dialog.Title>
+  );
 };
