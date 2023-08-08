23ae36b0331a957d0e411e55906902fbb2dd052f
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-08 16:53:04 -0300

    generate dynamic metadata

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,6 +1,36 @@
 import { userCanAccessProject } from '@/lib/project-guard';
+import { getServerAuthSession } from '@/server/auth';
+import { prisma } from '@/server/prisma';
+import { Metadata } from 'next';
 
-export default async function Page({ params }: { params: { projectId: string } }) {
+interface PageProps {
+  params: { projectId: string };
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    return {
+      title: '404 | Not Found'
+    };
+  }
+
+  const { projectId } = params;
+
+  const findProject = await prisma.project.findFirst({
+    where: {
+      id: projectId,
+      userId: session.user.id
+    }
+  });
+
+  return {
+    title: `Task-it | ${findProject?.name}`
+  };
+}
+
+export default async function Page({ params }: PageProps) {
   await userCanAccessProject(params.projectId);
 
   return (
