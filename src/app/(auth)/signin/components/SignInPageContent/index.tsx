1c8167fa76e5fbb0e4e762d2d9353f964d81429e
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-09 15:12:11 -0300

    add theme toggle to login page

--- src/app/(auth)/signin/components/SignInPageContent/index.tsx
+++ src/app/(auth)/signin/components/SignInPageContent/index.tsx
@@ -5,38 +5,39 @@ import { Icons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
 
 import { signIn } from 'next-auth/react';
+import { ThemeToggle } from '../ThemeToggle';
 
 export const SignInPageContent = () => {
   return (
     <main className="flex h-[100dvh] w-screen flex-col">
-      <TopBar />
+      <TopBar userActions={ThemeToggle} />
       <section className="flex h-full items-center justify-center space-x-8 space-y-5 px-5">
         <div className="flex w-full max-w-sm flex-col items-center justify-center space-y-3">
-          <h1 className="text-2xl">Sign in to continue</h1>
+          <h1 className="text-2xl">Continue with:</h1>
           <div className="flex w-full flex-col space-y-3 rounded-lg border border-solid border-muted p-5">
-            <Button className="gap-3" variant="secondary" onClick={() => signIn('google')}>
-              <Icons.google className="h-5 w-5" />
-              <p className="text-md">Continue with Google</p>
+            <Button className="gap-2" variant="outline" onClick={() => signIn('google')}>
+              <Icons.google className="h-4 w-4" />
+              <p className="text-md">Google</p>
             </Button>
-            <Button className="gap-3" variant="secondary" onClick={() => signIn('github')}>
-              <Icons.github className="h-5 w-5" />
-              <p className="text-md">Continue with Github</p>
+            <Button className="gap-2" variant="outline" onClick={() => signIn('github')}>
+              <Icons.github className="h-4 w-4" />
+              <p className="text-md">Github</p>
             </Button>
-            <Button className="gap-3" variant="secondary" onClick={() => signIn('github')}>
-              <Icons.discord className="h-5 w-5" />
-              <p className="text-md">Continue with Discord</p>
+            <Button className="gap-2" variant="outline" onClick={() => signIn('github')}>
+              <Icons.discord className="h-4 w-4" />
+              <p className="text-md">Discord</p>
             </Button>
-            <Button className="gap-3" variant="secondary" onClick={() => signIn('facebook')}>
-              <Icons.facebook className="h-5 w-5" />
-              <p className="text-md">Continue with Facebook</p>
+            <Button className="gap-2" variant="outline" onClick={() => signIn('facebook')}>
+              <Icons.facebook className="h-4 w-4" />
+              <p className="text-md">Facebook</p>
             </Button>
-            <Button className="gap-3" variant="secondary" onClick={() => signIn('42-school')}>
-              <Icons.school42 className="h-5 w-5" />
-              <p className="text-md">Continue with 42</p>
+            <Button className="gap-2" variant="outline" onClick={() => signIn('42-school')}>
+              <Icons.school42 className="h-4 w-4" />
+              <p className="text-md">42 School</p>
             </Button>
-            <Button className="gap-3" variant="secondary" onClick={() => signIn('auth0')}>
-              <Icons.auth0 className="h-5 w-5" />
-              <p className="text-md">Continue with Auth0</p>
+            <Button className="gap-2" variant="outline" onClick={() => signIn('auth0')}>
+              <Icons.auth0 className="h-4 w-4" />
+              <p className="text-md">Auth0</p>
             </Button>
           </div>
         </div>
