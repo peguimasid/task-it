630e8a88ce3f97da65f6e545719758025506fa4a
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-11 19:00:19 -0300

    improve login page

--- src/app/(auth)/signin/components/SignInPageContent/index.tsx
+++ src/app/(auth)/signin/components/SignInPageContent/index.tsx
@@ -15,28 +15,28 @@ export const SignInPageContent = () => {
         <div className="flex w-full max-w-sm flex-col items-center justify-center space-y-3">
           <h1 className="text-2xl">Continue with:</h1>
           <div className="flex w-full flex-col space-y-3 rounded-lg border border-solid border-muted p-5">
-            <Button className="gap-2" variant="outline" onClick={() => signIn('google')}>
-              <Icons.google className="h-4 w-4" />
+            <Button className="gap-2" variant="outline" size="lg" onClick={() => signIn('google')}>
+              <Icons.google className="h-5 w-5" />
               <p className="text-md">Google</p>
             </Button>
-            <Button className="gap-2" variant="outline" onClick={() => signIn('github')}>
-              <Icons.github className="h-4 w-4" />
+            <Button className="gap-2" variant="outline" size="lg" onClick={() => signIn('github')}>
+              <Icons.github className="h-5 w-5" />
               <p className="text-md">Github</p>
             </Button>
-            <Button className="gap-2" variant="outline" onClick={() => signIn('github')}>
-              <Icons.discord className="h-4 w-4" />
+            <Button className="gap-2" variant="outline" size="lg" onClick={() => signIn('github')}>
+              <Icons.discord className="h-5 w-5" />
               <p className="text-md">Discord</p>
             </Button>
-            <Button className="gap-2" variant="outline" onClick={() => signIn('facebook')}>
-              <Icons.facebook className="h-4 w-4" />
+            <Button className="gap-2" variant="outline" size="lg" onClick={() => signIn('facebook')}>
+              <Icons.facebook className="h-5 w-5" />
               <p className="text-md">Facebook</p>
             </Button>
-            <Button className="gap-2" variant="outline" onClick={() => signIn('42-school')}>
-              <Icons.school42 className="h-4 w-4" />
+            <Button className="gap-2" variant="outline" size="lg" onClick={() => signIn('42-school')}>
+              <Icons.school42 className="h-5 w-5" />
               <p className="text-md">42 School</p>
             </Button>
-            <Button className="gap-2" variant="outline" onClick={() => signIn('auth0')}>
-              <Icons.auth0 className="h-4 w-4" />
+            <Button className="gap-2" variant="outline" size="lg" onClick={() => signIn('auth0')}>
+              <Icons.auth0 className="h-5 w-5" />
               <p className="text-md">Auth0</p>
             </Button>
           </div>
