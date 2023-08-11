10e53a037580d77e50dbea0b61afb672271b94ad
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-11 19:09:06 -0300

    use next t3 env to validate environment variables

--- src/env.mjs
+++ src/env.mjs
@@ -1,51 +1,45 @@
+import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
-const validatePort = (value) => {
-  const parsedValue = Number(value);
-  return !isNaN(parsedValue) && Number.isInteger(parsedValue);
-};
-
-const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']),
-  // Next Auth Config
-  NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string(),
-  // Next Auth Google Provider
-  GOOGLE_CLIENT_ID: z.string(),
-  GOOGLE_CLIENT_SECRET: z.string(),
-  // Next Auth Discord Provider
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
-  // Next Auth Facebook Provider
-  FACEBOOK_CLIENT_ID: z.string(),
-  FACEBOOK_CLIENT_SECRET: z.string(),
-  // Next Auth Github Provider
-  GITHUB_CLIENT_ID: z.string(),
-  GITHUB_CLIENT_SECRET: z.string(),
-  // Next Auth Auth0 Provider
-  AUTH0_CLIENT_ID: z.string(),
-  AUTH0_CLIENT_SECRET: z.string(),
-  AUTH0_DOMAIN: z.string(),
-  // Next Auth 42 School Provider
-  FORTY_TWO_CLIENT_ID: z.string(),
-  FORTY_TWO_CLIENT_SECRET: z.string(),
-  // Prisma DB
-  POSTGRES_HOST: z.string(),
-  POSTGRES_PORT: z.string().refine(validatePort, 'Expected a valid port number'),
-  POSTGRES_USER: z.string(),
-  POSTGRES_PASSWORD: z.string(),
-  POSTGRES_DB: z.string(),
-  DATABASE_URL: z.string().url()
-});
-
-export const env = (() => {
-  try {
-    return envSchema.parse(process.env);
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      const errorMessages = error.errors.map(({ path, message }) => `👉 ${path.join('')}: ${message}`);
-      throw new Error(`❌ Missing or incorrect environment variables:\n${errorMessages.join('\n')}`);
-    }
-    throw error;
+export const env = createEnv({
+  server: {
+    NEXTAUTH_URL: z.string().url().optional(),
+    NEXTAUTH_SECRET: z.string().min(1),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
+    DISCORD_CLIENT_ID: z.string().min(1),
+    DISCORD_CLIENT_SECRET: z.string().min(1),
+    FACEBOOK_CLIENT_ID: z.string().min(1),
+    FACEBOOK_CLIENT_SECRET: z.string().min(1),
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1),
+    AUTH0_CLIENT_ID: z.string().min(1),
+    AUTH0_CLIENT_SECRET: z.string().min(1),
+    AUTH0_DOMAIN: z.string().min(1),
+    FORTY_TWO_CLIENT_ID: z.string().min(1),
+    FORTY_TWO_CLIENT_SECRET: z.string().min(1),
+    DATABASE_URL: z.string().min(1)
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().min(1)
+  },
+  runtimeEnv: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
+    FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    FORTY_TWO_CLIENT_ID: process.env.FORTY_TWO_CLIENT_ID,
+    FORTY_TWO_CLIENT_SECRET: process.env.FORTY_TWO_CLIENT_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL
   }
-})();
+});
--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { env } from '@/env.mjs';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -8,9 +7,9 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
   });
 
-if (env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
 }
