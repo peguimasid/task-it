4bd4697c5f8b6f2cc209a3bc55a84641412410b2
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-12 21:30:58 -0300

    feat: remove header from signin page

--- src/app/(auth)/signin/layout.tsx
+++ src/app/(auth)/signin/layout.tsx
@@ -1,9 +1,6 @@
 import { ReactNode } from 'react';
 import { Metadata } from 'next';
 
-import { ModeToggle } from '@/components/mode-toggle';
-import { TopBar } from '@/components/top-bar';
-
 export const metadata: Metadata = {
   title: 'Task-it | SignIn'
 };
@@ -11,7 +8,6 @@ export const metadata: Metadata = {
 export default function SignInLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-[100dvh] w-screen flex-col">
-      <TopBar userActions={ModeToggle} />
       <main className="flex-1">{children}</main>
     </div>
   );
