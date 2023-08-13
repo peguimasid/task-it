855ae1473336c2b07e2f5391eb894ffc984f748e
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-13 17:43:53 -0300

    feat(signin): improve user experience in login page by adding diabled and loading states

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,12 +1,32 @@
 'use client';
 
+import { useCallback, useMemo, useState } from 'react';
 import Link from 'next/link';
+import type { OAuthProviderType } from 'next-auth/providers';
 import { signIn } from 'next-auth/react';
 
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
 
 export default function Page() {
+  const [isLoading, setIsLoading] = useState({
+    google: false,
+    github: false,
+    discord: false,
+    facebook: false,
+    auth0: false,
+    '42-school': false
+  });
+
+  const disabled = useMemo(() => {
+    return Object.values(isLoading).some((val) => val === true);
+  }, [isLoading]);
+
+  const login = useCallback(async (selectedProvider: OAuthProviderType) => {
+    setIsLoading((providers) => ({ ...providers, [selectedProvider]: true }));
+    await signIn(selectedProvider);
+  }, []);
+
   return (
     <>
       <div className="relative h-full w-full">
@@ -23,12 +43,20 @@ export default function Page() {
             <p className="text-sm text-muted-foreground">Sign in with your preferred provider</p>
           </div>
           <div className="flex w-full flex-col space-y-3 rounded-lg border border-solid border-muted p-5">
-            <Button className="gap-2" variant="secondary" onClick={() => signIn('google')}>
-              <Icons.google className="h-5 w-5" />
+            <Button disabled={disabled} className="gap-2" variant="secondary" onClick={() => login('google')}>
+              {isLoading.google ? (
+                <Icons.spinner className="h-5 w-5 animate-spin" />
+              ) : (
+                <Icons.google className="h-5 w-5" />
+              )}
               <p>Google</p>
             </Button>
-            <Button className="gap-2" variant="secondary" onClick={() => signIn('github')}>
-              <Icons.github className="h-5 w-5" />
+            <Button disabled={disabled} className="gap-2" variant="secondary" onClick={() => login('github')}>
+              {isLoading.github ? (
+                <Icons.spinner className="h-5 w-5 animate-spin" />
+              ) : (
+                <Icons.github className="h-5 w-5" />
+              )}
               <p>Github</p>
             </Button>
             <div className="relative">
@@ -40,17 +68,57 @@ export default function Page() {
               </div>
             </div>
             <div className="flex w-full flex-row justify-center space-x-3">
-              <Button className="gap-2" variant="secondary" size="icon" onClick={() => signIn('discord')}>
-                <Icons.discord className="h-5 w-5" />
+              <Button
+                disabled={disabled}
+                className="gap-2"
+                variant="secondary"
+                size="icon"
+                onClick={() => login('discord')}
+              >
+                {isLoading.discord ? (
+                  <Icons.spinner className="h-5 w-5 animate-spin" />
+                ) : (
+                  <Icons.discord className="h-5 w-5" />
+                )}
               </Button>
-              <Button className="gap-2" variant="secondary" size="icon" onClick={() => signIn('facebook')}>
-                <Icons.facebook className="h-5 w-5" />
+              <Button
+                disabled={disabled}
+                className="gap-2"
+                variant="secondary"
+                size="icon"
+                onClick={() => login('facebook')}
+              >
+                {isLoading.facebook ? (
+                  <Icons.spinner className="h-5 w-5 animate-spin" />
+                ) : (
+                  <Icons.facebook className="h-5 w-5" />
+                )}
               </Button>
-              <Button className="gap-2" variant="secondary" size="icon" onClick={() => signIn('42-school')}>
-                <Icons.school42 className="h-5 w-5" />
+              <Button
+                disabled={disabled}
+                className="gap-2"
+                variant="secondary"
+                size="icon"
+                onClick={() => login('42-school')}
+              >
+                {isLoading['42-school'] ? (
+                  <Icons.spinner className="h-5 w-5 animate-spin" />
+                ) : (
+                  <Icons.school42 className="h-5 w-5" />
+                )}
               </Button>
-              <Button className="gap-2" variant="secondary" size="icon" onClick={() => signIn('auth0')}>
-                <Icons.auth0 className="h-5 w-5" />
+              <Button
+                disabled={disabled}
+                className="gap-2"
+                variant="secondary"
+                size="icon"
+                onClick={() => login('auth0')}
+              >
+                {isLoading.auth0 ? (
+                  <Icons.spinner className="h-5 w-5 animate-spin" />
+                ) : (
+                  <Icons.auth0 className="h-5 w-5" />
+                )}
               </Button>
             </div>
           </div>
