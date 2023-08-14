0d41f814e615728eed8c0edeab15ce7097c5e193
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-14 19:20:50 -0300

    feat: fetch project for user with tasks

--- src/app/(projects)/projects/[projectId]/page.tsx
+++ src/app/(projects)/projects/[projectId]/page.tsx
@@ -1,8 +1,38 @@
+import { notFound } from 'next/navigation';
+import { Project } from '@prisma/client';
+
+import { getServerAuthSession } from '@/lib/auth';
+import { prisma } from '@/lib/prisma';
+
 interface PageProps {
   params: { projectId: string };
 }
 
+const getProjectForUser = async (projectId: Project['id']): Promise<Project | null> => {
+  const session = await getServerAuthSession();
+
+  if (!session) return null;
+
+  const project = await prisma.project.findFirst({
+    where: {
+      id: projectId,
+      userId: session?.user?.id
+    },
+    include: {
+      tasks: true
+    }
+  });
+
+  return project;
+};
+
 export default async function Page({ params }: PageProps) {
+  const project = await getProjectForUser(params.projectId);
+
+  if (!project) {
+    return notFound();
+  }
+
   return (
     <div className="flex h-full">
       <h1>{params.projectId}</h1>
