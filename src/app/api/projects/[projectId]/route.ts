3e654220da1906fc2626426edd39d89f6c628a27
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-19 16:02:31 -0300

    feat: create endpoint to update project info

---
+++ src/app/api/projects/[projectId]/route.ts
@@ -0,0 +1,59 @@
+import { NextResponse } from 'next/server';
+import { Project } from '@prisma/client';
+import { z } from 'zod';
+
+import { getServerAuthSession } from '@/lib/auth';
+import { prisma } from '@/lib/prisma';
+import { projectPatchSchema } from '@/lib/validations/project';
+
+const routeContextSchema = z.object({
+  params: z.object({
+    projectId: z.string()
+  })
+});
+
+export async function PATCH(request: Request, context: z.infer<typeof routeContextSchema>) {
+  try {
+    const { params } = routeContextSchema.parse(context);
+
+    const userCanAccessProject = await userHasAccessToProject(params.projectId);
+
+    if (!userCanAccessProject) {
+      return NextResponse.json({ error: 'You cannot access this route' }, { status: 403 });
+    }
+
+    const json = await request.json();
+    const { name, description } = projectPatchSchema.parse(json);
+
+    await prisma.project.update({
+      where: {
+        id: params.projectId
+      },
+      data: {
+        name,
+        description
+      }
+    });
+
+    return NextResponse.json(null, { status: 204 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(JSON.stringify(error.issues), { status: 422 });
+    }
+
+    return NextResponse.json(null, { status: 500 });
+  }
+}
+
+async function userHasAccessToProject(projectId: Project['id']): Promise<boolean> {
+  const session = await getServerAuthSession();
+
+  const count = await prisma.project.count({
+    where: {
+      id: projectId,
+      userId: session?.user.id
+    }
+  });
+
+  return count > 0;
+}
---
+++ src/lib/validations/project.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod';
+
+export const projectPatchSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required' }).max(30, { message: 'Name can have at most 30 characters' }),
+  description: z.string().max(200, { message: 'Can you keep under 200 characters please?' }).optional()
+});
