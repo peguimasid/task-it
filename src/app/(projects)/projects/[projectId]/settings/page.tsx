dbb4586b261e62870097087ab5fc7ea1e18caab2
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-18 21:26:59 -0300

    feat: create project data form

--- src/app/(projects)/projects/[projectId]/settings/page.tsx
+++ src/app/(projects)/projects/[projectId]/settings/page.tsx
@@ -1,11 +1,43 @@
+import { redirect } from 'next/navigation';
+import { Project } from '@prisma/client';
+
+import { getServerAuthSession } from '@/lib/auth';
+import { prisma } from '@/lib/prisma';
+import { ProjectDataForm } from '@/components/project-data-form';
+
 interface PageProps {
   params: { projectId: string };
 }
 
+const findProjectById = async (projectId: string): Promise<Project | null> => {
+  const session = await getServerAuthSession();
+
+  if (!session) return null;
+
+  const project = await prisma.project.findFirst({
+    where: {
+      id: projectId,
+      userId: session?.user?.id
+    }
+  });
+
+  return project;
+};
+
 export default async function Page({ params }: PageProps) {
+  const project = await findProjectById(params.projectId);
+
+  if (!project) {
+    redirect('/projects');
+  }
+
   return (
-    <main className="flex h-full">
-      <h1>Settings {params.projectId}</h1>
+    <main className="flex h-full flex-col space-y-5 px-10 md:pl-0 md:pr-10">
+      <div className="space-y-2">
+        <h1 className="font-heading text-3xl md:text-4xl">Settings</h1>
+        <p className="text-lg text-muted-foreground">Mange project settings</p>
+      </div>
+      <ProjectDataForm project={project} />
     </main>
   );
 }
---
+++ src/components/project-data-form.tsx
@@ -0,0 +1,132 @@
+'use client';
+
+import { useCallback } from 'react';
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Project as PrismaProject } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
+import { isEmpty } from 'lodash';
+import { Loader2 } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import { toast } from './ui/use-toast';
+
+const projectSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required' }).max(30, { message: 'Name can have at most 30 characters' }),
+  description: z.string().max(200, { message: 'Can you keep under 200 characters please?' }).optional()
+});
+
+type FormValues = z.infer<typeof projectSchema>;
+
+type Project = Pick<PrismaProject, 'id' | 'name' | 'description'>;
+
+interface CreateProjectResponse {
+  newProject: Project;
+}
+
+const createProject = async (data: FormValues): Promise<CreateProjectResponse> => {
+  console.log(data);
+
+  return { newProject: { id: '123', name: 'TODO', description: '' } };
+};
+
+interface ProjectDataFormProps {
+  project: Project;
+}
+
+export const ProjectDataForm = ({ project }: ProjectDataFormProps) => {
+  const router = useRouter();
+
+  const defaultValues = {
+    name: project.name,
+    description: project.description
+  };
+
+  const form = useForm<FormValues>({
+    mode: 'onChange',
+    defaultValues,
+    resolver: zodResolver(projectSchema)
+  });
+
+  const { isValid, dirtyFields } = form.formState;
+
+  const onSuccess = useCallback(() => {
+    toast({
+      title: 'Success',
+      description: 'You project info has been updated'
+    });
+    router.refresh();
+  }, [router]);
+
+  const onError = useCallback(() => {
+    toast({
+      title: 'Something went wrong.',
+      description: 'Your project was not updated. Please try again.',
+      variant: 'destructive'
+    });
+  }, []);
+
+  const { isLoading, mutate } = useMutation({
+    mutationFn: createProject,
+    onSuccess,
+    onError
+  });
+
+  const isSubmitButtonDisabled = isEmpty(dirtyFields) || !isValid || isLoading;
+
+  const onSubmit = useCallback((formData: FormValues) => mutate(formData), [mutate]);
+
+  return (
+    <Form {...form}>
+      <form name="createProjectForm" noValidate onSubmit={form.handleSubmit(onSubmit)}>
+        <Card>
+          <CardHeader>
+            <CardTitle>Project</CardTitle>
+            <CardDescription>Enter the project name and description that you most like</CardDescription>
+          </CardHeader>
+          <CardContent className="max-w-lg space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} autoComplete="off" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input {...field} autoComplete="off" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isSubmitButtonDisabled}>
+              <Loader2
+                data-loading={isLoading}
+                className="mr-2 hidden h-4 w-4 animate-spin data-[loading=true]:block"
+              />
+              Save
+            </Button>
+          </CardContent>
+        </Card>
+      </form>
+    </Form>
+  );
+};
