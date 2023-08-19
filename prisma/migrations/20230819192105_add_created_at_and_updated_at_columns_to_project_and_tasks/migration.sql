e44fbf812e517d51399b03c0032ce2f698d414ce
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-19 16:53:28 -0300

    feat: add createdAt and updatedAt columns to project and tasks

---
+++ prisma/migrations/20230819192105_add_created_at_and_updated_at_columns_to_project_and_tasks/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Project` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Task` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Task" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- DropEnum
+DROP TYPE "TaskPriority";
+
+-- DropEnum
+DROP TYPE "TaskSize";
