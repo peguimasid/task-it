2576f7971aa8ad4becc34d1b715f721f4196a119
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-26 17:33:25 -0300

    feat: scroll looks good

--- src/app/(projects)/projects/[projectId]/page.tsx
+++ src/app/(projects)/projects/[projectId]/page.tsx
@@ -35,18 +35,30 @@ export default async function Page({ params }: PageProps) {
 
   return (
     <div className="flex h-full w-full">
-      <main className="flex h-full flex-col space-y-5 overflow-hidden px-5">
+      <main className="flex h-full w-full flex-col space-y-5 overflow-hidden px-5">
         <div className="space-y-2">
           <h1 className="font-heading text-3xl md:text-4xl">{project.name}</h1>
           <p className="text-muted-foreground">{project.description}</p>
         </div>
         <div className="h-full w-full rounded-md border">
-          <div className="flex h-full w-full gap-4 overflow-x-auto p-3">
-            <div className="h-full min-w-[300px] rounded-sm bg-muted"></div>
-            <div className="h-full min-w-[300px] rounded-sm bg-muted"></div>
-            <div className="h-full min-w-[300px] rounded-sm bg-muted"></div>
-            <div className="h-full min-w-[300px] rounded-sm bg-muted"></div>
-            <div className="h-full min-w-[300px] rounded-sm bg-muted"></div>
+          <div className="flex h-full w-full gap-4 p-3">
+            {/* COLUM */}
+            <div className="h-full min-w-[300px] space-y-3 rounded-sm bg-muted p-3">
+              <div className="flex w-full flex-row justify-between">
+                <h1>TODO</h1>
+                <button>+</button>
+              </div>
+              <div id="task" className="h-20 w-full bg-blue-900" />
+              <div id="task" className="h-20 w-full bg-blue-900" />
+              <div id="task" className="h-20 w-full bg-blue-900" />
+              <div id="task" className="h-20 w-full bg-blue-900" />
+              <div id="task" className="h-20 w-full bg-blue-900" />
+              <div id="task" className="h-20 w-full bg-blue-900" />
+              <div id="task" className="h-20 w-full bg-blue-900" />
+              <div id="task" className="h-20 w-full bg-blue-900" />
+              <div id="task" className="h-20 w-full bg-blue-900" />
+            </div>
+            {/* COLUM */}
           </div>
         </div>
       </main>
