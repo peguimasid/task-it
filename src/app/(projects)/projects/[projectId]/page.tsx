fe5f9c1c11c3fdff73bc789f588a8b0083c38e07
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-26 17:50:09 -0300

    feat: creat kanban board component

--- src/app/(projects)/projects/[projectId]/page.tsx
+++ src/app/(projects)/projects/[projectId]/page.tsx
@@ -3,12 +3,13 @@ import { Project } from '@prisma/client';
 
 import { getServerAuthSession } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
+import { KanbanBoard } from '@/components/kanban-board';
 
 interface PageProps {
   params: { projectId: string };
 }
 
-const getProjectForUser = async (projectId: Project['id']): Promise<Project | null> => {
+const getProjectForUser = async (projectId: Project['id']) => {
   const session = await getServerAuthSession();
 
   if (!session) return null;
@@ -40,27 +41,7 @@ export default async function Page({ params }: PageProps) {
           <h1 className="font-heading text-3xl md:text-4xl">{project.name}</h1>
           <p className="text-muted-foreground">{project.description}</p>
         </div>
-        <div className="h-full w-full rounded-md border">
-          <div className="flex h-full w-full gap-4 overflow-x-auto p-3">
-            {/* COLUM */}
-            <div className="h-full min-w-[300px] space-y-3 rounded-sm bg-muted p-3">
-              <div className="flex w-full flex-row justify-between">
-                <h1>TODO</h1>
-                <button>+</button>
-              </div>
-              <div id="task" className="h-20 w-full bg-blue-900" />
-              <div id="task" className="h-20 w-full bg-blue-900" />
-              <div id="task" className="h-20 w-full bg-blue-900" />
-              <div id="task" className="h-20 w-full bg-blue-900" />
-              <div id="task" className="h-20 w-full bg-blue-900" />
-              <div id="task" className="h-20 w-full bg-blue-900" />
-              <div id="task" className="h-20 w-full bg-blue-900" />
-              <div id="task" className="h-20 w-full bg-blue-900" />
-              <div id="task" className="h-20 w-full bg-blue-900" />
-            </div>
-            {/* COLUM */}
-          </div>
-        </div>
+        <KanbanBoard />
       </main>
     </div>
   );
---
+++ src/components/kanban-board.tsx
@@ -0,0 +1,25 @@
+export const KanbanBoard = () => {
+  return (
+    <div className="h-full w-full rounded-md border">
+      <div className="flex h-full w-full gap-4 overflow-x-auto p-3">
+        {/* COLUM */}
+        <div className="h-full min-w-[300px] space-y-3 rounded-sm bg-muted p-3">
+          <div className="flex w-full flex-row justify-between">
+            <h1>TODO</h1>
+            <button>+</button>
+          </div>
+          <div id="task" className="h-20 w-full bg-blue-900" />
+          <div id="task" className="h-20 w-full bg-blue-900" />
+          <div id="task" className="h-20 w-full bg-blue-900" />
+          <div id="task" className="h-20 w-full bg-blue-900" />
+          <div id="task" className="h-20 w-full bg-blue-900" />
+          <div id="task" className="h-20 w-full bg-blue-900" />
+          <div id="task" className="h-20 w-full bg-blue-900" />
+          <div id="task" className="h-20 w-full bg-blue-900" />
+          <div id="task" className="h-20 w-full bg-blue-900" />
+        </div>
+        {/* COLUM */}
+      </div>
+    </div>
+  );
+};
