24a7980b3ed3c0f5429314aac13e1d3d46696b97
Guilhermo Masid <guilhermomasid@gmail.com>
2023-09-11 18:17:15 -0300

    refactor: remove comment

--- src/components/kanban-board.tsx
+++ src/components/kanban-board.tsx
@@ -2,7 +2,6 @@ export const KanbanBoard = () => {
   return (
     <div className="h-full w-full rounded-lg border">
       <div className="flex h-full w-full gap-4 overflow-x-auto p-3">
-        {/* COLUM */}
         <div className="h-full min-w-[300px] space-y-3 rounded-sm bg-muted p-3">
           <div className="flex w-full flex-row justify-between">
             <h1>TODO</h1>
@@ -18,7 +17,6 @@ export const KanbanBoard = () => {
           <div id="task" className="h-20 w-full bg-blue-900" />
           <div id="task" className="h-20 w-full bg-blue-900" />
         </div>
-        {/* COLUM */}
       </div>
     </div>
   );
