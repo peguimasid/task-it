d6e1100dd7d57aadd0c6ef812c87f218afa6741f
Guilhermo Masid <guilhermomasid@gmail.com>
2023-09-13 11:17:15 -0300

    refactor: change title

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -5,10 +5,10 @@ import { CheckSquare } from 'lucide-react';
 export const TopBar = ({ children }: PropsWithChildren) => {
   return (
     <header className="sticky inset-0 z-10 flex h-20 w-full border-b border-muted backdrop-blur-md">
-      <div className="container mx-auto flex w-full flex-row items-center px-5 py-4">
-        <Link as="/" href="/" className="flex flex-row items-center justify-center gap-1 py-2">
+      <div className="container mx-auto flex w-full flex-row items-center">
+        <Link as="/" href="/" className="flex flex-row items-center justify-center gap-1">
           <CheckSquare className="h-6 w-6" />
-          <h1 className="font-heading text-xl font-extrabold">Task-it</h1>
+          <h1 className="font-heading text-xl font-extrabold">task-it</h1>
         </Link>
         <div className="ml-auto flex flex-row space-x-4">{children}</div>
       </div>
