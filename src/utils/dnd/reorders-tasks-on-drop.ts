1d6c71e08c3e0aef043c9e907f200a6e14fbc486
Guilhermo Masid <guilhermomasid@gmail.com>
2023-09-29 13:36:51 -0300

    feat: reorder tasks between columns working

--- src/utils/dnd/reorders-tasks-on-drop.ts
+++ src/utils/dnd/reorders-tasks-on-drop.ts
@@ -1,16 +1,7 @@
 import { DropResult } from '@hello-pangea/dnd';
 import { Task } from '@prisma/client';
 
-function reorderInSameColumn(dropResult: DropResult, tasks: Task[], movedTask: Task) {
-  const { destination, source } = dropResult;
-
-  const columnTasks = tasks.filter((task) => task.status === destination?.droppableId);
-
-  columnTasks.splice(source.index, 1);
-  columnTasks.splice(destination!.index, 0, movedTask);
-
-  const columnTasksReordered = columnTasks.map((task, index) => ({ ...task, index }));
-
+function updateTasks(tasks: Task[], columnTasksReordered: Task[]) {
   const tasksReordered = tasks?.map((task) => {
     const updatedTask = columnTasksReordered.find((ticket) => ticket.id === task.id);
 
@@ -27,6 +18,42 @@ function reorderInSameColumn(dropResult: DropResult, tasks: Task[], movedTask: T
   return tasksReordered ?? [];
 }
 
+function reorderInSameColumn(dropResult: DropResult, tasks: Task[], movedTask: Task) {
+  const { destination, source } = dropResult;
+
+  const columnTasks = tasks.filter((task) => task.status === destination?.droppableId);
+
+  columnTasks.splice(source.index, 1);
+  columnTasks.splice(destination!.index, 0, movedTask);
+
+  const columnTasksReordered = columnTasks.map((task, index) => ({ ...task, index }));
+
+  return updateTasks(tasks, columnTasksReordered);
+}
+
+function reorderBetweenColumns(dropResult: DropResult, tasks: Task[], movedTask: Task) {
+  const { destination, source } = dropResult;
+
+  const sourceColumnTasks = tasks.filter((task) => task.status === source.droppableId);
+  const destinationColumnTasks = tasks.filter((task) => task.status === destination!.droppableId);
+
+  sourceColumnTasks.splice(source.index, 1);
+  destinationColumnTasks.splice(destination!.index, 0, movedTask);
+
+  const sourceColumnTasksReordered = sourceColumnTasks.map((task, index) => ({
+    ...task,
+    index
+  }));
+
+  const destinationColumnTasksReordered = destinationColumnTasks.map((task, index) => ({
+    ...task,
+    index,
+    status: destination!.droppableId
+  }));
+
+  return updateTasks(tasks, [...sourceColumnTasksReordered, ...destinationColumnTasksReordered]);
+}
+
 export function reorderTasksOnDrop(dropResult: DropResult, tasks: Task[]) {
   const { destination, source, draggableId } = dropResult;
 
@@ -41,8 +68,7 @@ export function reorderTasksOnDrop(dropResult: DropResult, tasks: Task[]) {
   const movedTask = tasks.find((task) => task.id === draggableId) as Task;
 
   if (hasChangedColumn) {
-    // return reorderBetweenColumns(dropResult, tasks, movedTask);
-    return;
+    return reorderBetweenColumns(dropResult, tasks, movedTask);
   }
 
   return reorderInSameColumn(dropResult, tasks, movedTask);
