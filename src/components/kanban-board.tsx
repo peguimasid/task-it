50d4d6023e72da27b45be7f720ad13c869e20352
Guilhermo Masid <guilhermomasid@gmail.com>
2023-09-29 15:02:48 -0300

    feat: drop task mutation working

--- src/components/kanban-board.tsx
+++ src/components/kanban-board.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
 import { statuses } from '@/constants/task-statuses';
 import { reorderTasksOnDrop } from '@/utils/dnd/reorders-tasks-on-drop';
 import { DragDropContext, DropResult } from '@hello-pangea/dnd';
-import { Task } from '@prisma/client';
+import { Project, Task } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
 
 import { StatusColumn } from './status-column';
 
@@ -12,13 +14,34 @@ interface KanbanBoardProps {
   tasks: Task[];
 }
 
+interface ReorderTasksProps {
+  reorderedTasks: Task[];
+  projectId: Project['id'];
+}
+
+const reorderTasks = async ({ projectId, reorderedTasks }: ReorderTasksProps): Promise<void> => {
+  await fetch(`/api/projects/${projectId}/tasks`, {
+    method: 'PATCH',
+    body: JSON.stringify({ tasks: reorderedTasks })
+  });
+};
+
 export const KanbanBoard = ({ tasks: initialState }: KanbanBoardProps) => {
+  const router = useRouter();
+  const { projectId }: { projectId: string } = useParams();
+
   const [tasks, setTasks] = useState(initialState);
+
+  const dropTaskMutation = useMutation({
+    mutationFn: reorderTasks,
+    onSettled: router.refresh
+  });
 
   const onDragEnd = (dropResult: DropResult) => {
     const reorderedTasks = reorderTasksOnDrop(dropResult, tasks);
     if (!reorderedTasks?.length) return;
     setTasks(reorderedTasks);
+    dropTaskMutation.mutate({ projectId, reorderedTasks });
   };
 
   return (
