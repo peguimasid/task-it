0edf65d74844180d7ddd32239757568ad9ea910c
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-02 21:21:15 -0300

    fix: back with old top bar style so dont break marketing page

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -5,7 +5,7 @@ import { CheckSquare } from 'lucide-react';
 export const TopBar = ({ children }: PropsWithChildren) => {
   return (
     <header className="sticky inset-0 z-10 flex h-20 w-full border-b backdrop-blur-md">
-      <div className="mx-auto flex w-full max-w-4xl flex-row items-center px-5">
+      <div className="container flex flex-row items-center">
         <Link as="/" href="/" className="flex flex-row items-center justify-center gap-1">
           <CheckSquare className="h-6 w-6" />
           <h1 className="font-heading text-xl font-extrabold">task-it</h1>
