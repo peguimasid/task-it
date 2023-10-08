6c39fa662fd7fc34e49b39eba699732a1ed795d0
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-08 13:21:30 -0300

    chore: back with sample tasks

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,6 +1,40 @@
 import { getServerAuthSession } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
 
+const sampleTasks = [
+  {
+    title: 'Create your first task',
+    status: 'BACKLOG',
+    index: 0,
+    priority: 'Medium',
+    tags: ['Beginner'],
+    size: 'Small'
+  },
+  {
+    title: 'Write your first task description',
+    status: 'BACKLOG',
+    index: 1,
+    priority: 'Low',
+    size: 'Large'
+  },
+  {
+    title: 'Explore Task-it platform',
+    status: 'IN_PROGRESS',
+    index: 0,
+    priority: 'High',
+    tags: ['Explore'],
+    size: 'Medium'
+  },
+  {
+    title: 'Bring your project to life',
+    status: 'DONE',
+    index: 0,
+    priority: 'Urgent',
+    tags: ['Achievement'],
+    size: 'Tiny'
+  }
+];
+
 export async function POST(request: Request) {
   const session = await getServerAuthSession();
 
@@ -21,6 +55,18 @@ export async function POST(request: Request) {
       }
     }
   });
+
+  const userProjectsCount = await prisma.project.count({
+    where: {
+      userId: session.user.id
+    }
+  });
+
+  if (userProjectsCount === 1) {
+    await prisma.task.createMany({
+      data: sampleTasks.map((task) => ({ ...task, projectId: newProject.id }))
+    });
+  }
 
   return new Response(JSON.stringify({ newProject }));
 }
