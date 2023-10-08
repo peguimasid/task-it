681beac22eaf6a878d7297309fbf374ec270fb93
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-08 16:59:35 -0300

    feat: improve design

--- src/app/(projects)/projects/[projectId]/loading.tsx
+++ src/app/(projects)/projects/[projectId]/loading.tsx
@@ -5,13 +5,16 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Loading() {
   return (
-    <main className="flex h-full w-full flex-col space-y-5">
+    <main className="flex h-full w-full flex-col space-y-8 pt-4">
       <div className="space-y-4">
         <Skeleton className="h-9 w-2/12 min-w-[200px] md:h-10" />
       </div>
       <div className="flex h-full w-full gap-3 overflow-x-auto">
         {Object.keys(statuses).map((status) => (
-          <div key={status} className="h-full w-[350px] min-w-[350px] rounded-lg border border-secondary bg-secondary">
+          <div
+            key={status}
+            className="h-full w-1/3 min-w-[350px] max-w-[400px] rounded-lg border border-secondary bg-secondary"
+          >
             <div className="flex w-full flex-row items-center justify-between rounded-t-lg bg-card p-3">
               <div className="flex items-center gap-2 py-1">
                 <Skeleton className="h-5 w-32 rounded-full" />
--- src/app/(projects)/projects/[projectId]/page.tsx
+++ src/app/(projects)/projects/[projectId]/page.tsx
@@ -38,7 +38,7 @@ export default async function Page({ params }: PageProps) {
   }
 
   return (
-    <main className="flex h-full w-full flex-col space-y-5">
+    <main className="flex h-full w-full flex-col space-y-8 pt-4">
       <h1 className="font-heading text-3xl sm:text-4xl">{project.name}</h1>
       <KanbanBoard projectId={project.id} tasks={project.tasks} />
     </main>
