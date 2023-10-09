70b04e88f7b2b8d258ea13c5e0ec63ee59e5da4a
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-09 14:35:57 -0300

    feat: structure folders to prevent layout from being shared with childrens

---
+++ src/app/(projects)/projects/[projectId]/deleted/page.tsx
@@ -0,0 +1,48 @@
+import { notFound } from 'next/navigation';
+import { Project, Task } from '@prisma/client';
+
+import { getServerAuthSession } from '@/lib/auth';
+import { prisma } from '@/lib/prisma';
+
+interface PageProps {
+  params: { projectId: string };
+}
+
+type ProjectWithTasks = Project & {
+  tasks: Task[];
+};
+
+const getProjectForUser = async (projectId: Project['id']): Promise<ProjectWithTasks | null> => {
+  const session = await getServerAuthSession();
+
+  if (!session) return null;
+
+  const project = await prisma.project.findFirst({
+    where: {
+      id: projectId,
+      userId: session?.user?.id
+    },
+    include: {
+      tasks: {
+        orderBy: { index: 'asc' },
+        where: { deletedAt: null }
+      }
+    }
+  });
+
+  return project;
+};
+
+export default async function Page({ params }: PageProps) {
+  const project = await getProjectForUser(params.projectId);
+
+  if (!project) {
+    return notFound();
+  }
+
+  return (
+    <main className="flex h-full w-full flex-col space-y-8 pt-4">
+      <h1>Page</h1>
+    </main>
+  );
+}
