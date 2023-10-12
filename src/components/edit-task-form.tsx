e5f613357fbc294e8051450be99275aae0fce4b8
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-12 09:54:34 -0300

    refactor: remove shadow

--- src/components/edit-task-form.tsx
+++ src/components/edit-task-form.tsx
@@ -8,7 +8,7 @@ export const EditTaskForm = ({ task }: EditTaskFormProps) => {
   return (
     <div className="flex flex-col gap-6">
       <div className="h-16 w-full rounded-lg border" />
-      <div className="relative mb-[calc(30vh)] min-h-[400px] w-full rounded-lg border shadow-lg"></div>
+      <div className="relative mb-[calc(30vh)] min-h-[400px] w-full rounded-lg border"></div>
     </div>
   );
 };
