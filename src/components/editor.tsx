1aa566b821b08fee086db8e52eabb29251896687
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-17 17:56:19 -0300

    feat: add code hightligh

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,15 +1,30 @@
 'use client';
 
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import css from 'highlight.js/lib/languages/css';
+import js from 'highlight.js/lib/languages/javascript';
+import ts from 'highlight.js/lib/languages/typescript';
+import html from 'highlight.js/lib/languages/xml';
+import { common, createLowlight } from 'lowlight';
 
-interface EditorProps {}
+import 'highlight.js/styles/github-dark.css';
 
-export const Editor = ({}: EditorProps) => {
+const lowlight = createLowlight(common);
+
+lowlight.register({ css, html, js, ts });
+
+export const Editor = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      CodeBlockLowlight.configure({
+        lowlight
+      })
+    ],
     content:
-      '<h1>Advantages of Tailwind CSS</h1> <p>Tailwind CSS is a utility-first CSS framework that offers several advantages for web development:</p> <blockquote>Tailwind is the best in the market</blockquote> <a href="https://github.com/peguimasid">Github</a> <ul> <li> <strong>Saves Time and Effort: </strong> <span>Tailwind classes enable rapid styling, reducing the need to write custom CSS.</span> </li> <li> <strong>Scalability: </strong> <span>Easily scale your design to fit the needs of your project.</span> </li> <li> <strong>Maintainability: </strong> <span>Clear and predictable class names make code maintainable and readable.</span> </li> </ul>',
+      '<h1>Advantages of Tailwind CSS</h1> <p>Tailwind CSS is a utility-first CSS framework that offers several advantages for web development:</p> <pre> <code class="language-javascript">function main() { \n\treturn "Hello World"\n } </code></pre> <a href="https://github.com/peguimasid">Github</a> <ul> <li> <strong>Saves Time and Effort: </strong> <span>Tailwind classes enable rapid styling, reducing the need to write custom CSS.</span> </li> <li> <strong>Scalability: </strong> <span>Easily scale your design to fit the needs of your project.</span> </li> <li> <strong>Maintainability: </strong> <span>Clear and predictable class names make code maintainable and readable.</span> </li> </ul>',
     editorProps: {
       attributes: {
         class: 'outline-none'
