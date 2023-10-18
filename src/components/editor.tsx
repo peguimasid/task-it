6efe8fd1cae32380910387c92119ba30be8156a2
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-18 18:55:57 -0300

    refactor: remove floating menu

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -2,23 +2,16 @@
 
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
 import Placeholder from '@tiptap/extension-placeholder';
-import { BubbleMenu, EditorContent, FloatingMenu, useEditor } from '@tiptap/react';
+import { BubbleMenu, EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import css from 'highlight.js/lib/languages/css';
-import js from 'highlight.js/lib/languages/javascript';
-import ts from 'highlight.js/lib/languages/typescript';
-import html from 'highlight.js/lib/languages/xml';
 import { common, createLowlight } from 'lowlight';
 
 import 'highlight.js/styles/github-dark.css';
 
 import { Icons } from './icons';
-import { ScrollArea } from './ui/scroll-area';
 import { Toggle } from './ui/toggle';
 
 const lowlight = createLowlight(common);
-
-lowlight.register({ css, html, js, ts });
 
 export const Editor = () => {
   const editor = useEditor({
@@ -46,70 +39,10 @@ export const Editor = () => {
 
   return (
     <>
-      <EditorContent className="prose prose-stone max-w-full dark:prose-invert" editor={editor} />
-      <FloatingMenu
+      <EditorContent
+        className="prose prose-stone max-w-full dark:prose-invert prose-pre:bg-secondary/40"
         editor={editor}
-        shouldShow={({ state }) => {
-          const { $from } = state.selection;
-          const currentLineText = $from.nodeBefore?.textContent;
-          return currentLineText === '/';
-        }}
-      >
-        <ScrollArea className="flex h-72 w-72 flex-col gap-1 rounded-lg border bg-card p-1 shadow-lg shadow-black/20">
-          <span className="px-1 text-xs font-semibold text-muted-foreground">Basic blocks</span>
-          <button
-            type="button"
-            className="group flex w-full flex-row items-center gap-2 rounded p-1 hover:bg-secondary"
-          >
-            <div className="flex h-10 w-10 items-center justify-center rounded border text-muted-foreground group-hover:border-primary/10">
-              <Icons.text className="h-5 w-5" />
-            </div>
-            <div className="flex flex-col text-left">
-              <span className="text-sm">Text</span>
-              <span className="text-xs text-muted-foreground">Just start writing with plain text.</span>
-            </div>
-          </button>
-          <button
-            type="button"
-            onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-            className="group flex w-full flex-row items-center gap-2 rounded p-1 hover:bg-secondary"
-          >
-            <div className="flex h-10 w-10 items-center justify-center rounded border text-muted-foreground group-hover:border-primary/10">
-              <Icons.heading1 className="h-5 w-5" />
-            </div>
-            <div className="flex flex-col text-left">
-              <span className="text-sm">Heading 1</span>
-              <span className="text-xs text-muted-foreground">Big section heading.</span>
-            </div>
-          </button>
-          <button
-            type="button"
-            onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-            className="group flex w-full flex-row items-center gap-2 rounded p-1 hover:bg-secondary"
-          >
-            <div className="flex h-10 w-10 items-center justify-center rounded border text-muted-foreground group-hover:border-primary/10">
-              <Icons.heading2 className="h-5 w-5" />
-            </div>
-            <div className="flex flex-col text-left">
-              <span className="text-sm">Heading 2</span>
-              <span className="text-xs text-muted-foreground">Medium section heading.</span>
-            </div>
-          </button>
-          <button
-            type="button"
-            onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-            className="group flex w-full flex-row items-center gap-2 rounded p-1 hover:bg-secondary"
-          >
-            <div className="flex h-10 w-10 items-center justify-center rounded border text-muted-foreground group-hover:border-primary/10">
-              <Icons.heading3 className="h-5 w-5" />
-            </div>
-            <div className="flex flex-col text-left">
-              <span className="text-sm">Heading 3</span>
-              <span className="text-xs text-muted-foreground">Small section heading.</span>
-            </div>
-          </button>
-        </ScrollArea>
-      </FloatingMenu>
+      />
       <BubbleMenu
         editor={editor}
         className="flex space-x-1 overflow-hidden rounded-lg border bg-card p-1 shadow-lg shadow-black/20"
