84c5b20664346dcebdc010226ea05ca3817f544a
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-20 20:44:03 -0300

    feat: set query param task on click task

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
+import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import { TaskPriority, TaskSize } from '@/types';
 import { Draggable } from '@hello-pangea/dnd';
 import { Task } from '@prisma/client';
 
+import { createUrl } from '@/lib/utils';
+
 import { TagsList } from './tags-list';
 import { TaskOperations } from './task-operations';
-import { TaskSheet } from './task-sheet';
 import { Card, CardTitle } from './ui/card';
 
 interface TaskCardProps {
@@ -16,33 +18,36 @@ interface TaskCardProps {
 }
 
 export const TaskCard = ({ task, index }: TaskCardProps) => {
+  const router = useRouter();
+  const { projectId }: { projectId: string } = useParams();
+
+  const searchParams = useSearchParams();
   const { title, priority, size, tags } = task;
 
-  const [isTaskSheetOpen, setIsTaskSheetOpen] = useState<boolean>(false);
-
   const openTaskSheet = useCallback(() => {
-    setIsTaskSheetOpen(true);
-  }, []);
+    const newParams = new URLSearchParams(searchParams.toString());
+
+    newParams.set('view', task.id);
+
+    router.push(createUrl(`/projects/${projectId}`, newParams));
+  }, [projectId, router, searchParams, task.id]);
 
   return (
-    <>
-      <Draggable draggableId={task.id} index={index}>
-        {(provided) => (
-          <Card
-            className="group relative mb-3 min-h-[6rem] space-y-2 border-none p-3"
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            <CardTitle className="cursor-pointer text-sm hover:underline" onClick={openTaskSheet}>
-              {title}
-            </CardTitle>
-            <TagsList priority={priority as TaskPriority} size={size as TaskSize} tags={tags} />
-            <TaskOperations task={task} />
-          </Card>
-        )}
-      </Draggable>
-      <TaskSheet task={task} isSheetOpen={isTaskSheetOpen} onSheetOpenChange={setIsTaskSheetOpen} />
-    </>
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <Card
+          className="group relative mb-3 min-h-[6rem] space-y-2 border-none p-3"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <CardTitle className="cursor-pointer text-sm hover:underline" onClick={openTaskSheet}>
+            {title}
+          </CardTitle>
+          <TagsList priority={priority as TaskPriority} size={size as TaskSize} tags={tags} />
+          <TaskOperations task={task} />
+        </Card>
+      )}
+    </Draggable>
   );
 };
