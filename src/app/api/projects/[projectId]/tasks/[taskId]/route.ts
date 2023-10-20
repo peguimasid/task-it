982b17ccca1162edfe7cf9ed82f926db2e45d868
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-20 12:22:41 -0300

    feat: handle update status in task

--- src/app/api/projects/[projectId]/tasks/[taskId]/route.ts
+++ src/app/api/projects/[projectId]/tasks/[taskId]/route.ts
@@ -15,23 +15,50 @@ export async function PUT(request: Request, context: z.infer<typeof routeContext
   try {
     const { params } = routeContextSchema.parse(context);
 
-    const userCanAccessProject = await userHasAccessToProject(params.projectId);
+    const { projectId, taskId } = params;
+
+    const userCanAccessProject = await userHasAccessToProject(projectId);
 
     if (!userCanAccessProject) {
       return new Response('You cannot access this route', { status: 403 });
     }
 
+    const currentTask = await prisma.task.findUnique({
+      where: {
+        id: taskId
+      }
+    });
+
     const json = await request.json();
     const updatedData = taskPutSchema.parse(json);
 
+    const changedStatus = currentTask?.status !== updatedData.status;
+
+    if (changedStatus) {
+      await prisma.task.updateMany({
+        where: { status: { equals: updatedData.status } },
+        data: { index: { increment: 1 } }
+      });
+
+      await prisma.task.updateMany({
+        where: {
+          status: { equals: currentTask?.status },
+          index: { gt: 0 }
+        },
+        data: { index: { decrement: 1 } }
+      });
+
+      updatedData.index = 0;
+    }
+
     const taskUpdated = await prisma.task.update({
-      where: { id: params.taskId },
+      where: { id: taskId },
       data: updatedData
     });
 
     await prisma.project.update({
       where: {
-        id: params.projectId
+        id: projectId
       },
       data: {
         updatedAt: new Date()
