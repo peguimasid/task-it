65639e1ea3bfabff9edb05e046b735e53f896f13
Guilhermo Masid <guilhermomasid@gmail.com>
2023-10-20 12:49:44 -0300

    feat: update working

--- src/components/task-sheet.tsx
+++ src/components/task-sheet.tsx
@@ -1,6 +1,9 @@
 import { useCallback, useMemo, useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import { useTaskStore } from '@/store/task-store';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Task } from '@prisma/client';
+import { Project, Task } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
 import { isEmpty } from 'lodash';
 import { FormProvider, useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -11,6 +14,7 @@ import { Icons } from '@/components/icons';
 
 import { EditTaskForm } from './edit-task-form';
 import { Form } from './ui/form';
+import { toast } from './ui/use-toast';
 
 const FormSchema = z.object({
   title: z
@@ -31,9 +35,32 @@ interface TaskSheetProps {
   onSheetOpenChange: (open: boolean) => void;
   task: Task;
 }
+interface UpdateTaskProps {
+  data: TaskSheetFormValues;
+  projectId: Project['id'];
+  taskId: Task['id'];
+}
+
+interface UpdateTaskResponse {
+  taskUpdated: Task;
+}
+
+const updateTask = async ({ data, projectId, taskId }: UpdateTaskProps): Promise<UpdateTaskResponse> => {
+  const response = await fetch(`/api/projects/${projectId}/tasks/${taskId}`, {
+    method: 'PUT',
+    body: JSON.stringify(data)
+  });
+  const responseData = await response.json();
+  return responseData;
+};
 
 export const TaskSheet = ({ task, isSheetOpen, onSheetOpenChange }: TaskSheetProps) => {
+  const router = useRouter();
+  const { projectId }: { projectId: string } = useParams();
+
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const onUpdateTask = useTaskStore((store) => store.onUpdateTask);
 
   const defaultValues = useMemo<TaskSheetFormValues>(() => {
     return {
@@ -67,14 +94,40 @@ export const TaskSheet = ({ task, isSheetOpen, onSheetOpenChange }: TaskSheetPro
     setIsExpanded((previousValue) => !previousValue);
   }, []);
 
-  const onSubmit = (formData: TaskSheetFormValues) => {
-    console.log(formData);
-  };
+  const onSuccess = useCallback(
+    ({ taskUpdated }: UpdateTaskResponse, variables: TaskSheetFormValues) => {
+      onUpdateTask(taskUpdated);
+      form.reset(variables);
+      router.refresh();
+    },
+    [form, onUpdateTask, router]
+  );
+
+  const onError = useCallback(() => {
+    toast({
+      title: 'Something went wrong.',
+      description: 'Your task was not updated. Please try again.',
+      variant: 'destructive'
+    });
+  }, []);
+
+  const { isLoading, mutate } = useMutation({
+    mutationFn: (data) => updateTask({ data, projectId, taskId: task.id }),
+    onSuccess,
+    onError
+  });
+
+  const onSubmit = useCallback(
+    (formData: TaskSheetFormValues) => {
+      mutate(formData);
+    },
+    [mutate]
+  );
 
   const isSaveButtonDisabled = useMemo(() => {
     const { isValid, dirtyFields } = form.formState;
-    return isEmpty(dirtyFields) || !isValid;
-  }, [form.formState]);
+    return isEmpty(dirtyFields) || !isValid || isLoading;
+  }, [form.formState, isLoading]);
 
   return (
     <Sheet open={isSheetOpen} onOpenChange={handleOpenChange}>
@@ -101,7 +154,7 @@ export const TaskSheet = ({ task, isSheetOpen, onSheetOpenChange }: TaskSheetPro
                 </Button>
                 <div className="ml-auto flex items-center gap-3">
                   <Button type="submit" disabled={isSaveButtonDisabled} className="h-9">
-                    <Icons.check className="mr-2 h-5 w-5" />
+                    {isLoading ? <Icons.spinner className="mr-2 h-5 w-5" /> : <Icons.check className="mr-2 h-5 w-5" />}
                     Save changes
                   </Button>
                   <SheetClose asChild>
